901253d3f27d06f4fb5d05ef63b23c38486fa379
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-10-15 09:08:20 -0300

    feat: local storage wrapper

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,58 @@
-const path = require('path');
+const path = require("path");
 
-const constantsFiles = ['RequestStatusEnum'];
+const constantsFiles = ["RequestStatusEnum"];
 const formattersFiles = [
-	'currency/brl',
-	'currency/brlPlain',
-	'currency/index',
-	'percentage/decimals',
-	'percentage/index',
+	"currency/brl",
+	"currency/brlPlain",
+	"currency/index",
+	"percentage/decimals",
+	"percentage/index",
 ];
 
 const helpersFiles = [
-	'axiosWrapper/index',
-	'errorBuilder/index',
-	'validators/cpfOrCnpj',
-	'validators/email',
-	'validators/index',
-	'notify/notification',
-	'notify/notificationWrapper',
-	'notify/index',
-	'store/gettersBuilder',
-	'store/index',
-	'store/userAccess/index',
-	'store/mutationsBuilder',
-	'store/resetListBuilder',
-	'fileSaver/index',
-	'string/capitalize',
-	'string/index',
-	'toClipboard/index',
+	"axiosWrapper/index",
+	"errorBuilder/index",
+	"validators/cpfOrCnpj",
+	"validators/email",
+	"validators/index",
+	"localStorageWrapper/index",
+	"notify/notification",
+	"notify/notificationWrapper",
+	"notify/index",
+	"store/gettersBuilder",
+	"store/index",
+	"store/userAccess/index",
+	"store/mutationsBuilder",
+	"store/resetListBuilder",
+	"fileSaver/index",
+	"string/capitalize",
+	"string/index",
+	"toClipboard/index",
 ];
-const mixinsFiles = ['gtag', 'routeRole'];
+const mixinsFiles = ["gtag", "routeRole"];
 
 let entry = {};
-constantsFiles.forEach(file => {
-	entry['consts/' + file] = `./src/consts/${file}.js`;
+constantsFiles.forEach((file) => {
+	entry["consts/" + file] = `./src/consts/${file}.js`;
 });
-formattersFiles.forEach(file => {
-	entry['formatters/' + file] = `./src/formatters/${file}.js`;
+formattersFiles.forEach((file) => {
+	entry["formatters/" + file] = `./src/formatters/${file}.js`;
 });
-helpersFiles.forEach(file => {
-	entry['helpers/' + file] = `./src/helpers/${file}.js`;
+helpersFiles.forEach((file) => {
+	entry["helpers/" + file] = `./src/helpers/${file}.js`;
 });
-mixinsFiles.forEach(file => {
-	entry['mixins/' + file] = `./src/mixins/${file}.js`;
+mixinsFiles.forEach((file) => {
+	entry["mixins/" + file] = `./src/mixins/${file}.js`;
 });
 
 module.exports = {
 	entry,
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: '[name].js',
-		libraryTarget: 'umd',
+		path: path.join(__dirname, "dist"),
+		filename: "[name].js",
+		libraryTarget: "umd",
 	},
 	resolve: {
-		extensions: ['.js'],
+		extensions: [".js"],
 	},
 };
