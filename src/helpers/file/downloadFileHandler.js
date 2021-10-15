1067b68ae5aa5052561f5d9e6fc49ae5ef874579
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-10-15 17:14:07 -0300

    fix: lint

--- src/helpers/file/downloadFileHandler.js
+++ src/helpers/file/downloadFileHandler.js
@@ -1,12 +1,11 @@
 export default (url) => {
 	const token = window.localStorage.getItem('Token');
-	const href = baseURL + url + '&token=' + token;
+	const href = `${url}&token=${token}`;
 	window.open(href, '_blank');
 
 	return new Promise((resolve) => {
 		resolve({
 			data: {
-				//No futuro terá um objeto com dados a serem apresentados
 				content: 'Download do arquivo foi iniciado na aba aberta',
 				status: 200,
 			},
