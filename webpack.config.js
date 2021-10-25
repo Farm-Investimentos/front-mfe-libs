30287619b401096d221c2e657cc94f100d9bc146
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-10-25 16:41:53 -0300

    chore: melhoria webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,23 @@
-const path = require("path");
+const path = require('path');
+const glob = require('glob');
 
-const constantsFiles = ["RequestStatusEnum"];
-const formattersFiles = [
-	"currency/brl",
-	"currency/brlPlain",
-	"currency/index",
-	"percentage/decimals",
-	"percentage/index",
-];
-
-const helpersFiles = [
-	"axiosWrapper/index",
-	"date/index",
-	"date/formatDateRangeSetKeys",
-	"date/defaultDateFormat",
-	"date/unFormatDate",
-	"date/dateStringToISO",
-	"errorBuilder/index",
-	"file/downloadFileHandler",
-	"file/index",
-	"validators/cpfOrCnpj",
-	"validators/email",
-	"validators/index",
-	"localStorageWrapper/index",
-	"notify/notification",
-	"notify/notificationWrapper",
-	"notify/index",
-	"store/gettersBuilder",
-	"store/index",
-	"store/userAccess/index",
-	"store/mutationsBuilder",
-	"store/resetListBuilder",
-	"fileSaver/index",
-	"string/capitalize",
-	"string/index",
-	"toClipboard/index"
-];
-const mixinsFiles = ["gtag", "routeRole", "exportHandler"];
-
-let entry = {};
-constantsFiles.forEach((file) => {
-	entry["consts/" + file] = `./src/consts/${file}.js`;
-});
-formattersFiles.forEach((file) => {
-	entry["formatters/" + file] = `./src/formatters/${file}.js`;
-});
-helpersFiles.forEach((file) => {
-	entry["helpers/" + file] = `./src/helpers/${file}.js`;
-});
-mixinsFiles.forEach((file) => {
-	entry["mixins/" + file] = `./src/mixins/${file}.js`;
-});
+const entry = glob
+	.sync('./src/**/*.js')
+	.filter((item) => item.indexOf('.spec.js') <= 0)
+	.reduce((acc, path) => {
+		const entry = path.replace('./src/', '');
+		acc[entry] = path;
+		return acc;
+	}, {});
 
 module.exports = {
 	entry,
 	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "[name].js",
-		libraryTarget: "umd",
+		path: path.join(__dirname, 'dist'),
+		filename: '[name]',
+		libraryTarget: 'umd',
 	},
 	resolve: {
-		extensions: [".js"],
+		extensions: ['.js'],
 	},
 };
