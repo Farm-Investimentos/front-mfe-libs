038c97e3fcca5d8e754f35f5a542588bab2b8a17
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2021-11-01 11:26:33 -0300

    docs: string

--- src/helpers/string/capitalize.js
+++ src/helpers/string/capitalize.js
@@ -1,8 +1,8 @@
 /**
  * Capitalize a string
  * @module
- * @param {s} - the string to be capitalized
- * @returns {string} - new string, capitalized
+ * @param {string} s - the string to be capitalized
+ * @returns {string} new string, capitalized
  */
 export default (s) => {
 	if (typeof s !== 'string') return '';
